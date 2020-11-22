5867e555e3fa9f443802012fd042a315bfec23aa
WanderingPhilosopher <65258474+WanderingPhilosopher@users.noreply.github.com>
2020-11-21 21:19:19 -0600

    Add files via upload

---
+++ combinetamefiles.bat
@@ -0,0 +1,5 @@
+start cmd cd C:\Users\KangarooHybrid\combinearea /k copy tame*.txt mytamefiles.txt
+
+pause
+
+change the "C:\Users\KangarooHybrid\combinearea" text above to match the folder location of your tame and wild files
---
+++ combinewildfiles.bat
@@ -0,0 +1,5 @@
+start cmd cd C:\Users\KangarooHybrid\combinearea /k copy wild*.txt mywildfiles.txt
+
+pause
+
+change the "C:\Users\KangarooHybrid\combinearea" text above to match the folder location of your tame and wild files
